5a3d2299ab2d3422b05120ebe0012a282b99067b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-22 19:14:17 +0900

    feat: Codeforces support

--- src/index.ts
+++ src/index.ts
@@ -78,7 +78,13 @@ function getAtCoderProblemCategory(problemId: string): string {
 
 function updateInfoAtCoder(info: HTMLUListElement, url: URL): void {
   const problem = lookupAtCoderProblem(url, info);
+  if (!problem) {
+    return;
+  }
   const contest = lookupAtCoderContest(problem["contest_id"], info);
+  if (!contest) {
+    return;
+  }
   const category = getAtCoderProblemCategory(problem["id"]);
   const contestUrl = "https://atcoder.jp/contests/" + contest["id"];
   addSpoiler(info, "Online Judge", "AtCoder", { url: "https://atcoder.jp/", icon: "link-45deg" });
@@ -90,8 +96,69 @@ function updateInfoAtCoder(info: HTMLUListElement, url: URL): void {
   addSpoiler(info, "URL", url.toString(), { url: url.toString(), icon: "link-45deg" });
 }
 
+function loadCodeforcesAPI(url: string, info: HTMLUListElement): any {
+  const req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send();
+  if (req.status != 200) {
+    addSpoiler(info, "error: failed to call API of Codeforces", req.statusText, { icon: "exclamation-triangle" });
+    return [];
+  }
+  const data = JSON.parse(req.responseText);
+  if (data["status"] != "OK") {
+    addSpoiler(info, "error: failed to call API of Codeforces", req.responseText, { icon: "exclamation-triangle" });
+    return [];
+  }
+  return data["result"];
+}
+
+function lookupCodeforcesProblem(url: URL, info: HTMLUListElement): { [key: string]: any } {
+  const match = /\/+contest\/+(\w+)\/+problem\/+(\w+)/.exec(url.pathname);
+  if (!match) {
+    addSpoiler(info, "error: failed to parse URL", "", { icon: "exclamation-triangle" });
+    return {};
+  }
+  const contestId = match[1];
+  const index = match[2];
+  const problems: { [key: string]: any }[] = loadCodeforcesAPI("https://codeforces.com/api/problemset.problems", info)["problems"];
+  for (const problem of problems) {
+    console.log(problem);
+    if (problem["contestId"] == contestId && problem["index"] == index) {
+      return problem;
+    }
+  }
+  console.log(contestId);
+  console.log(index);
+  addSpoiler(info, "error: problem info is not found in Codeforces", contestId + ", " + index, { icon: "exclamation-triangle" });
+  return {};
+}
+
+function lookupCodeforcesContest(contestId: number, info: HTMLUListElement): { [key: string]: any } {
+  const contests: { [key: string]: any }[] = loadCodeforcesAPI("https://codeforces.com/api/contest.list", info);
+  for (const contest of contests) {
+    if (contest["id"] == contestId) {
+      return contest;
+    }
+  }
+  return {};
+}
+
 function updateInfoCodeforces(info: HTMLUListElement, url: URL): void {
+  const problem = lookupCodeforcesProblem(url, info);
+  if (!problem) {
+    return;
+  }
+  const contest = lookupCodeforcesContest(problem["contestId"], info);
+  if (!contest) {
+    return;
+  }
+  const contestUrl = "https://codeforces.com/contest/" + contest["id"];
   addSpoiler(info, "Online Judge", "Codeforces", { url: "https://codeforces.com/", icon: "link-45deg" });
+  addSpoiler(info, "Contest", contest["name"], { url: contestUrl, icon: "link-45deg" });
+  addSpoiler(info, "Rating", problem["rating"], { icon: "bar-chart-line" });
+  addSpoiler(info, "Type", problem["type"], { icon: "bar-chart-line" });
+  addSpoiler(info, "Tags", problem["tags"].join(", "), { icon: "bar-chart-line" });
+  addSpoiler(info, "Problem Name", problem["index"] + ". " + problem["name"], { url: url.toString(), icon: "link-45deg" });
   addSpoiler(info, "URL", url.toString(), { url: url.toString(), icon: "link-45deg" });
 }
 
